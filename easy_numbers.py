17d0538459ee3b66f678752f921c710f0814aba4
ishootin <ishegor77@mail.ru>
2020-03-05 01:56:23 +0300

    А гит у меня кстати красивый

--- easy_numbers.py
+++ easy_numbers.py
@@ -1,13 +1,12 @@
 """Алгоритм для проверки числа на простоту"""
 from math import ceil, sqrt
-def is_simple(n: 'int') -> 'str':
+def is_simple(n: 'int'):
     if n == 1:
         return "Not true, Not false"
     for i in range (2, ceil(sqrt(n))+3):
         if n % i == 0:
-            return "False"
-        return "True"
-    
+            return False
+        return True
+   
 n= int(input("Число для проверочки на простоту "))
 print(is_simple(n))
-   
