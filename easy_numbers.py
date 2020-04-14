80451e1f6df37c718865eab8f9e74644388c45d3
ishootin <55479037+ishootin@users.noreply.github.com>
2020-04-14 10:58:29 +0300

    Update easy_numbers.py

--- easy_numbers.py
+++ easy_numbers.py
@@ -1,8 +1,10 @@
 """Алгоритм для проверки числа на простоту"""
 from math import ceil, sqrt
-def is_simple(n: 'int'):
+from typing import Optional
+
+def is_simple(n: int)-> Optional[bool]:
     if n == 1:
-        return "Not true, Not false"
+        return None
     for i in range (2, ceil(sqrt(n))+3):
         if n % i == 0:
             return False
